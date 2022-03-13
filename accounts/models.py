1b2f21cb9ad4901c8f93d21f8498765618dcabdb
Harshit Paneri <82382478+harshit-paneri@users.noreply.github.com>
2022-03-13 09:12:37 +0530

    Add files via upload

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class user_accounts(models.Model):
+    email=models.EmailField(max_length=200,primary_key=True)
+    user_pass=models.CharField(max_length=200,null=False)
