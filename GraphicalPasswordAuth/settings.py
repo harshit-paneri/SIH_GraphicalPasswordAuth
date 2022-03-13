26d16381d07a684070a962bd53b45edf12249123
Harshit Paneri <82382478+harshit-paneri@users.noreply.github.com>
2022-03-13 09:13:29 +0530

    Updated files

--- GraphicalPasswordAuth/settings.py
+++ GraphicalPasswordAuth/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig',
+    'mathfilters',
 ]
 
 MIDDLEWARE = [
@@ -117,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
