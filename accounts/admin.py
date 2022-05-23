a52be887c08689463b80989912f6f2907c200a61
Priyanshu Arora <priyanshuarora595@gmail.com>
2022-05-23 23:23:05 +0530

    clear for reset link

--- accounts/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from accounts.models import user_accounts
-# Register your models here.
-admin.site.register(user_accounts)
--- codes/segmentation.py
+++
@@ -1,6 +0,0 @@
-from image_slicer import slice
-import os
-
-file_name=input("Enter file name (with path) = ")
-size=int(input("enter number pieces wyou want = "))
-slice(file_name,size)
